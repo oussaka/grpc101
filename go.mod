17cdd55bc7fc0859b5dd2ff53d2a616d98ecf7ae
oussaka <kaabachi.dev@gmail.com>
2025-07-19 19:58:10 +0200

    [misc.] - add zerolog

--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250715232539-7130f93afb79 // indirect
--- server.go
+++ server.go
@@ -4,29 +4,34 @@ package main
 import (
 	"fmt"
 	"github.com/ensep/grpc101/handler"
-	"log"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc/reflection"
 	"net"
 
 	"github.com/ensep/grpc101/proto/profile"
 	"google.golang.org/grpc"
 )
 
-func main() {
+const (
+	GrpcServerAddress string = "0.0.0.0:9000"
+)
 
+func main() {
 	fmt.Println("Go gRPC 101 Teknasyon!")
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	lis, err := net.Listen("tcp", GrpcServerAddress)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal().Err(err).Msgf("failed to listen: %v", err)
 	}
 
 	s := handler.Profile{}
 
 	grpcServer := grpc.NewServer()
-
+	reflection.Register(grpcServer)
 	profile.RegisterProfileServer(grpcServer, &s)
 
+	log.Info().Msgf("start gRPC server at %s", lis.Addr().String())
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
+		log.Fatal().Err(err).Msgf("failed to serve: %s", err)
 	}
 }
